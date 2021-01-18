f7018d440dd07038c45abf28c011e2e6ed26b8d9
B Bryant <no_mail@b3sk.com>
2021-01-18 12:53:42 -0500

    Initial Commit

---
+++ ControlFlowTest/ControlFlowTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlFlowTestApp.swift
+//  ControlFlowTest
+//
+//  Created by Barry Bryant on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ControlFlowTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
